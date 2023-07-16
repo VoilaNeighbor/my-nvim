0e71cf0ef2d5069c928c5d5e831e9979602672b6
Fifnmar <fifnmar@gmail.com>
2023-07-16 22:13:09 +0800

    Add LSP plugins

--- init.lua
+++ init.lua
@@ -12,3 +12,48 @@ vim.cmd [[
 	set shellquote=\"
 	set shellxquote=
 ]]
+
+
+local function ensure_lazy_nvim()
+	local path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+	if not vim.loop.fs_stat(path) then
+		vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", path })
+	end
+	vim.opt.rtp:prepend(path)
+end
+
+
+ensure_lazy_nvim()
+
+
+-- Sometimes, lazy.nvim cannot setup a plugin automatically.
+local plugins = {
+	{
+		'williamboman/mason.nvim',
+		-- lazy.nvim cannot setup Mason automatically. The default config function does not work.
+		config = function() require 'mason'.setup() end,
+		build = ':MasonUpdate',
+	},
+
+	{
+		'neovim/nvim-lspconfig',
+		config = function()
+			local lspconfig = require 'lspconfig'
+			lspconfig.lua_ls.setup {
+				settings = {
+					Lua = {
+						runtime = { version = 'LuaJIT' },
+						diagnostics = { globals = { 'vim' } },
+						telemetry = { enable = false },
+						workspace = {
+							library = vim.api.nvim_get_runtime_file("", true),
+						},
+					},
+				},
+			}
+		end
+	},
+}
+
+require 'lazy'.setup(plugins, {})
+
